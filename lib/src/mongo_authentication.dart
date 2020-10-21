bac0d9d396fb7b1135aa9054e20d6ae3cebe58d5
pamu47 <pamudithamaduranga@gmail.com>
2020-10-21 19:08:37 +0530

    fixed forgotPwd method and random code generator added

--- lib/src/mongo_authentication.dart
+++ lib/src/mongo_authentication.dart
@@ -1,15 +1,15 @@
-//implemented just to check the functionality of the mongo_user model class.
-
+import 'dart:math';
 import 'package:mAuth/mAuth.dart';
+import 'package:mAuth/src/services.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:crypto/crypto.dart';
 import 'dart:convert';
-import '../mAuth.dart';
 import 'mongo_user.dart';
 
 class MongoAuth {
   static Db db = MongoDB.instance;
   var collection = db.collection("dartCollection");
+  Services services = new Services();
 
   addCollection(MongoUser user) async {
     final MongoUser usermodel = new MongoUser(
@@ -18,14 +18,27 @@ class MongoAuth {
     await collection.insert(data);
   }
 
-  Future<MongoUser> forgotPassword(String email) async {
-    await collection.findOne({"email": email}).then((value) {
-      print(value);
-      MongoUser user = MongoUser.fromMap(value);
-      print(user.name);
-      return user;
-    });
+  Future<String> forgotPassword(String email) async {
+    try {
+      Map<String, dynamic> doc = await collection.findOne({"email": email});
+      if (doc == null) {
+        return "NO_USER";
+      } else {
+        doc["tempCode"] = services.tempCodeGenerator();
+        await collection.save(doc);
+        return "USER_FOUND";
+        // ignore: todo
+        //TODO: send email.
+      }
+    } on MongoDartError catch (e) {
+      print(e.message);
+      return e.message;
+    } catch (e) {
+      print(e);
+      return e.toString();
+    }
   }
+
 
   hashPassword(String password) {
     var key = utf8.encode(password);
